e544b93b287bfba8460d9a23dbf83b19f4393ce4
C. Masloch <pushbx@ulukai.org>
2021-11-20 14:06:29 +0100

    sen: add s1opt switch for passing --sleepduration to s1.sh

--- Software/asm/sen.sh
+++ Software/asm/sen.sh
@@ -6,10 +6,17 @@
 #
 # DISCLAIMER: THE WORKS ARE WITHOUT WARRANTY.
 
+s1opt=""
+if [[ "$1" == --* ]]
+then
+  s1opt="$1"
+  shift
+fi
+
 source="$1"
 shift
 
 nasm "$source" -f ith -o output.hex "$@"
 (($?)) && exit $?
 
-(echo "l:020000020000FC"; cat output.hex) | sed -re 's/:00000001FF/:0400000300000100F8/g' | tr -d '\n' | ./s1.pl -
+(echo "l:020000020000FC"; cat output.hex) | sed -re 's/:00000001FF/:0400000300000100F8/g' | tr -d '\n' | ./s1.pl $s1opt -
